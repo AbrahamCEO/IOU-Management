04acc23b5fc3b8ba8f73f8c439f545a0a807c929
AMN <87907130+AbrahamCEO@users.noreply.github.com>
2024-07-30 09:55:24 +0200

    database_service.dart

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,6 +1,6 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
+import 'package:sqflite/sqlite_api.dart';t
 
 class DatabaseService {
   static Database? _db;
